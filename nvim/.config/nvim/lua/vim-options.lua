5a44b78cd42b02349cf7687f4831e723c3dd2903
ParsaJR <parsajr@proton.me>
2025-01-24 16:14:34 +0330

    nvim: some configuration for tabs...

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -1,3 +1,30 @@
+-- Some general configuration that does not relate to any specific plugin!
+
 vim.cmd("set number")
 vim.g.mapleader = " "
 vim.opt.clipboard = "unnamedplus"
+
+function _G.get_tabline_string()
+	local tabline_string = ""
+	for tabnum = 1, vim.fn.tabpagenr("$") do
+		local winnr = vim.fn.tabpagewinnr(tabnum)
+		local bufferlist = vim.fn.tabpagebuflist(tabnum)[winnr]
+		local buffername = vim.fn.bufname(bufferlist)
+		-- remove the buffername absolute path from the buffername. here, :t means tail of the file name
+		local buffername_short = vim.fn.fnamemodify(buffername, ":t")
+		-- check if it is the active tab
+		if tabnum == vim.fn.tabpagenr() then
+			tabline_string = tabline_string ..
+			"%#TabLineSel#" .. " " .. tabnum .. ": " .. buffername_short .. " "
+		else
+			tabline_string = tabline_string ..
+			"%#TabLine#" .. " " .. tabnum .. ": " .. buffername_short .. " "
+		end
+
+	end
+	tabline_string = tabline_string .. "%#TabLineFill#"
+	return tabline_string
+end
+
+vim.o.tabline = "%!v:lua.get_tabline_string()"
+vim.o.showtabline = 2
