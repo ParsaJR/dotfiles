0ecec3688be0dfaf5b0a9738dc459544915f4faa
ParsaJR <parsajr@proton.me>
2025-01-27 22:55:42 +0330

    nvim: minor

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -56,9 +56,8 @@ return {
 			lspconfig.cssls.setup({
 				capabilities = capabilities,
 			})
-			local vue_language_server_path = mason_registry.get_package("vue-language-server")
-			    :get_install_path()
-			    .. "/node_modules/@vue/language-server"
+			local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
+				.. "/node_modules/@vue/language-server"
 			lspconfig.ts_ls.setup({
 				init_options = {
 					plugins = {
