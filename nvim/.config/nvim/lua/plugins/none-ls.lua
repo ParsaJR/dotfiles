7b4a06010970329e640c33b5f462bb19cdb05dc5
ParsaJR <parsajr@proton.me>
2025-01-29 14:32:45 +0330

    nvim: i dont need eslint for now

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -10,7 +10,6 @@ return {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.shfmt,
-				require("none-ls.diagnostics.eslint_d"),
 			},
 		})
 
