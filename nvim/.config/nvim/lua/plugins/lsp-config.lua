d3651f1e2c2324069da7b03e0b22cb5b458245a4
ParsaJR <parsajr@proton.me>
2025-05-15 22:57:31 +0330

    nvim: golang unimport improvements

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -51,6 +51,30 @@ return {
 						completeUnimported = true,
 					},
 				},
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					pattern = "*.go",
+					callback = function()
+						local params = vim.lsp.util.make_range_params()
+						params.context = { only = { "source.organizeImports" } }
+						-- buf_request_sync defaults to a 1000ms timeout. Depending on your
+						-- machine and codebase, you may want longer. Add an additional
+						-- argument after params if you find that you have to write the file
+						-- twice for changes to be saved.
+						-- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+						local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction",
+							params)
+						for cid, res in pairs(result or {}) do
+							for _, r in pairs(res.result or {}) do
+								if r.edit then
+									local enc = (vim.lsp.get_client_by_id(cid) or {})
+									.offset_encoding or "utf-16"
+									vim.lsp.util.apply_workspace_edit(r.edit, enc)
+								end
+							end
+						end
+						vim.lsp.buf.format({ async = false })
+					end,
+				}),
 			})
 			lspconfig.html.setup({
 				capabilities = capabilities,
@@ -142,7 +166,9 @@ return {
 					})
 				end,
 			})
-			local vue_language_server_path = vim.fn.expand '$MASON/packages' .. '/vue-language-server' .. '/node_modules/@vue/language-server' 
+			local vue_language_server_path = vim.fn.expand("$MASON/packages")
+			    .. "/vue-language-server"
+			    .. "/node_modules/@vue/language-server"
 			lspconfig.ts_ls.setup({
 				init_options = {
 					plugins = {
@@ -169,11 +195,12 @@ return {
 					vim.cmd("tabmove $")
 				end
 			end
+
 			vim.keymap.set("n", "<F5>", RunDev, { noremap = true, silent = true })
 			vim.keymap.set("n", "H", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n","<leader>e",vim.diagnostic.open_float)
+			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 		end,
 	},
 }
