e0ff4b7c0a4707a17f0c2f5b210550eb04a5f089
ParsaJR <parsajr@proton.me>
2025-05-23 14:56:34 +0330

    minor

--- scripts/scripts/dmenu-firefox-search.sh
+++ scripts/scripts/dmenu-firefox-search.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Term="$(echo "" | dmenu)"
+Term="$(echo "" | dmenu -p "Search something in google:")"
 if [ ! -z $Term ]; then
 	firefox --new-window "https://www.google.com/search?q=$Term" 
 fi
