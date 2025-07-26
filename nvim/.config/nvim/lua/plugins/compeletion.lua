872597d4e720090423ee0d57b326c3c40e639422
ParsaJR <parsajr@proton.me>
2025-07-26 21:46:57 +0330

    nvim compeltion docs

--- nvim/.config/nvim/lua/plugins/compeletion.lua
+++ nvim/.config/nvim/lua/plugins/compeletion.lua
@@ -47,8 +47,8 @@ return {
 				},
 				-- The keybinding for completion window
 				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-k>"] = cmp.mapping.scroll_docs(-4),
+					["<C-j>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = confirm, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
