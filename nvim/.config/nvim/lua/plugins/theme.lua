f1253fec4bbcc99f719fa42b532376cfc89adcc8
ParsaJR <parsajr@proton.me>
2025-08-25 20:20:21 +0330

    nvim: change back to catpuccin

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,13 +1,13 @@
--- return {
--- 	"catppuccin/nvim", name = "catppuccin", priority = 1000,
--- 	config = function()
--- 		vim.cmd.colorscheme "catppuccin"
--- 	end
--- }
 return {
-	"mcauley-penney/techbase.nvim",
-	config = function(_, opts)
-		vim.cmd.colorscheme("techbase")
-	end,
-	priority = 1000,
+	"catppuccin/nvim", name = "catppuccin", priority = 1000,
+	config = function()
+		vim.cmd.colorscheme "catppuccin"
+	end
 }
+-- return {
+-- 	"mcauley-penney/techbase.nvim",
+-- 	config = function(_, opts)
+-- 		vim.cmd.colorscheme("techbase")
+-- 	end,
+-- 	priority = 1000,
+-- }
