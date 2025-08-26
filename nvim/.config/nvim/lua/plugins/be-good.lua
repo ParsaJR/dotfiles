6bb56d39a264911f86a04a180faf0a4341188418
ParsaJR <parsajr@proton.me>
2025-08-26 06:50:27 +0330

    nvim: clean up

--- nvim/.config/nvim/lua/plugins/be-good.lua
+++
@@ -1,4 +0,0 @@
-return {
-	-- It's a game for getting better at vim
-	"ThePrimeagen/vim-be-good"
-}
---
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -0,0 +1,12 @@
+return {
+	{
+		-- It's a game for getting better at vim
+		"ThePrimeagen/vim-be-good",
+	},
+	{
+		"m4xshen/hardtime.nvim",
+		lazy = false,
+		dependencies = { "MunifTanjim/nui.nvim" },
+		opts = {},
+	},
+}
