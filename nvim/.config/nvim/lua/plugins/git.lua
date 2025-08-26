79194962da405e5b11c8f21e4bfba43ea168152f
ParsaJR <parsajr@proton.me>
2025-08-26 07:01:48 +0330

    nvim: new plguin vim surround

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -1,6 +1,13 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	config = function()
-		require("gitsigns").setup()
-	end,
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
+	{
+		"tpope/vim-surround",
+		config = function()
+		end,
+	},
 }
