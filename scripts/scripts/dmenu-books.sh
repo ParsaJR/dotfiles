bcb71a0cd7c98a9901fffa7a8e29a1680949f482
ParsaJR <parsajr@proton.me>
2025-09-04 20:52:29 +0330

    scripts: i3 keybinding

--- scripts/scripts/dmenu-books.sh
+++ scripts/scripts/dmenu-books.sh
@@ -2,11 +2,9 @@
 
 # I read Ebooks a lot. this script will help me to open them fast by just typing the name of the file.
 
-
 LIBRARY_PATH="/home/parsa/Documents/books/"
 
 cd $LIBRARY_PATH
-
 
 CHOICES=$(find . -type f -printf "%f\n")
 
@@ -14,10 +12,11 @@ CHOICE=$(echo -e "$CHOICES" | dmenu -i -p "What would you like to read?")
 
 BOOK_PATH=$(find $LIBRARY_PATH -type f -iname "$CHOICE")
 
-echo -e $BOOK_PATH
 
+if [ -n "$BOOK_PATH" ]; then 
+	zathura "$BOOK_PATH"
+fi
 
-zathura "$BOOK_PATH"
 
 
 
