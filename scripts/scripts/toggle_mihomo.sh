2f494d9ed40b44c8cf5857b8cf156416e27659b5
ParsaJR <parsajr@proton.me>
2025-10-10 14:23:24 +0330

    fix: minor

--- scripts/scripts/toggle_mihomo.sh
+++ scripts/scripts/toggle_mihomo.sh
@@ -2,14 +2,14 @@
 
 # Simple script for toggling the mihomo (clash.meta) service.
 
+# Suppose we have the systemd service already setup for the service:
 SERVICE="mihomo.service"
 
 if [ "$(systemctl is-active "$SERVICE")" = "active" ]; then
 	sudo systemctl stop $SERVICE
+	notify-send "mihomo stopped."	
 else
 	sudo systemctl start $SERVICE
+	notify-send "mihomo started."	
 fi
 
-if [ $? -eq 0 ]; then
-	notify-send done!	
-fi
