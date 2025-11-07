caaf1b147907eb310c45d2955b561574db038b83
ParsaJR <parsajr@proton.me>
2025-11-07 11:39:09 +0330

    nvim: disable autoclose for file type "vimwiki"

--- nvim/.config/nvim/lua/plugins/autoclose.lua
+++ nvim/.config/nvim/lua/plugins/autoclose.lua
@@ -5,7 +5,7 @@ return {
 			require("autoclose").setup({
 				options = {
 					disable_command_mode = true,
-					disabled_filetypes = { "text", "markdown" },
+					disabled_filetypes = { "text", "markdown", "vimwiki" },
 				},
 			})
 		end,
