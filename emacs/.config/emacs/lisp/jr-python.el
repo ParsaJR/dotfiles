ec34805e21eee366ffb2ca5c902dbb28c0c56a67
ParsaJR <parsajr@proton.me>
2026-06-08 14:49:57 +0330

    emacs: uv-mode

--- emacs/.config/emacs/lisp/jr-python.el
+++ emacs/.config/emacs/lisp/jr-python.el
@@ -7,6 +7,10 @@
 ;;   (add-to-list 'eglot-server-programs
 ;;                '((python-ts-mode) "basedpyright-langserver" "--stdio")))
 
+(use-package uv-mode
+  :ensure t
+  :hook (python-ts-mode . uv-mode-auto-activate-hook))
+
 (with-eval-after-load 'eglot
   (add-to-list 'eglot-server-programs
                '(python-mode . ("rass" "basedruff") )))
