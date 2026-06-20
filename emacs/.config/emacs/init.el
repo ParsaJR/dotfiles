60ab70b45298506bcb18aa865b010a15b4879e09
ParsaJR <parsajr@proton.me>
2026-06-20 14:06:19 +0330

    emacs: Move the custom vars out of the init.el

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -22,16 +22,6 @@
 
 (load "jr-golang")
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(help-window-select t)
- '(package-selected-packages nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+(setq custom-file (locate-user-emacs-file "custom-vars.el"))
+(load custom-file 'noerror 'nomessage)
